442b27aa349457a604702530f3b6af2f5a852817
naponknot <naponknot@gmail.com>
2018-04-26 23:34:59 +0700

    add changes for the last whole section of the course

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://naponknot:password@ds239009.mlab.com:39009/blog_dev',
   cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIAIL5MLIVZ2HXKCGWQ',
+  secretAccessKey: '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'
 };
